042689ae11cd149cdd1955f55b30f26373551458
osipiuqar <raquipiso.rha@gmail.com>
2025-05-01 00:47:18 +0800

    navbar and sidebar: wip

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SidenavComponent } from '../sidenav/sidenav.component';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SidenavComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  navLinks = [
-    { label: 'Home', path: '/' },
-    { label: 'Menu', path: '/menu' },
-    { label: 'Contact Us', path: '/contact-us' },
-  ];
+  isNavbarVisible: boolean = false;
+
+  toggleNavbar(): void {
+    this.isNavbarVisible = !this.isNavbarVisible;
+  }
 }
