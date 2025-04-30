25e5e30d669e875b21e762b45396ae023bb4eb63
Rain Raquipiso <87350585+rainraqui@users.noreply.github.com>
2025-04-30 17:07:32 +0800

    Initial commit - SYNCO cafe website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
